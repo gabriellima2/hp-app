e856f6f5c3bce21cf8a31f26a58599c01de045b1
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-03-08 17:54:31 -0300

    feat: adiciona novos elementos à página Home

--- src/core/presentation/views/Home/Home.test.tsx
+++ src/core/presentation/views/Home/Home.test.tsx
@@ -1,11 +1,19 @@
 import { render } from "@testing-library/react-native";
+
 import { Home } from "./Home";
+import { listHouses } from "../../../../__mocks__";
+
+const TITLE_TEXT = "Explore os personagens de Harry Potter";
 
 describe("<Home />", () => {
 	describe("Render", () => {
 		it("should render correctly", () => {
 			const { getByText } = render(<Home />);
-			expect(getByText("Explore os personagens de Harry Potter")).toBeTruthy();
+
+			expect(getByText(TITLE_TEXT)).toBeTruthy();
+			listHouses((house) => {
+				expect(getByText(house)).toBeTruthy();
+			});
 		});
 	});
 });
--- src/core/presentation/views/Home/Home.tsx
+++ src/core/presentation/views/Home/Home.tsx
@@ -1,10 +1,11 @@
-import { Title } from "../../components";
+import { Title, HousesList } from "../../components";
 import { AppLayout } from "../../layouts";
 
 export const Home = () => {
 	return (
 		<AppLayout>
 			<Title>Explore os personagens de Harry Potter</Title>
+			<HousesList />
 		</AppLayout>
 	);
 };
