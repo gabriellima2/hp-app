8289ee79cb8b37b2d00e39c47477766a569ac8d2
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-03-08 15:40:30 -0300

    feat: adiciona componentes globais de texto

---
+++ src/core/presentation/components/Paragraph.tsx
@@ -0,0 +1,15 @@
+import { Text, TextProps, StyleSheet } from "react-native";
+import { themes } from "../../../styles/themes";
+
+export const Paragraph = (props: TextProps) => (
+	<Text style={[styles.paragraph, props.style]}>{props.children}</Text>
+);
+
+const styles = StyleSheet.create({
+	paragraph: {
+		color: themes.fontColor,
+		fontWeight: "400",
+		opacity: 0.8,
+		fontSize: 14,
+	},
+});
---
+++ src/core/presentation/components/Title.tsx
@@ -0,0 +1,14 @@
+import { Text, TextProps, StyleSheet } from "react-native";
+import { themes } from "../../../styles/themes";
+
+export const Title = (props: TextProps) => (
+	<Text style={[styles.title, props.style]}>{props.children}</Text>
+);
+
+const styles = StyleSheet.create({
+	title: {
+		color: themes.fontColor,
+		fontWeight: "700",
+		fontSize: 20,
+	},
+});
