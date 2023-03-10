47105457eca3404d8e1f62d235bd732a2b13132e
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-03-10 16:20:11 -0300

    feat: adiciona estrutura inicial da tela Details

---
+++ src/core/presentation/views/Details/Details.tsx
@@ -0,0 +1,8 @@
+import { Title } from "@/core/presentation/components";
+import { AppLayout } from "@/core/presentation/layouts";
+
+export const Details = () => (
+	<AppLayout>
+		<Title>Details</Title>
+	</AppLayout>
+);
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,6 +2,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import { MakeHome } from "../core/main/factories/pages/make-home";
+import { MakeDetails } from "../core/main/factories/pages/make-details";
+
 import type { StackParams } from "../shared/@types/StackParams";
 
 const Stack = createStackNavigator<StackParams>();
@@ -10,13 +12,8 @@ export const Routes = () => {
 	return (
 		<NavigationContainer>
 			<Stack.Navigator screenOptions={{ headerShown: false }}>
-				<Stack.Screen
-					name="Home"
-					component={MakeHome}
-					options={{
-						title: "Home",
-					}}
-				/>
+				<Stack.Screen name="Home" component={MakeHome} />
+				<Stack.Screen name="Details" component={MakeDetails} />
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
